c3228f7afd026a3ff663c47a35fbee9a1604bf51
Kite <kite.nguyen@pal.net.vn>
2026-03-30 09:22:03 +0700

    Change to Signal Input for CodePresenter

--- src/app/common/components/code-presenter/code-presenter.ts
+++ src/app/common/components/code-presenter/code-presenter.ts
@@ -5,10 +5,10 @@ import {
   DoCheck,
   inject,
   Injector,
+  input,
   Input,
   PLATFORM_ID,
-  Signal,
-  signal
+  Signal
 } from '@angular/core';
 import {CommonModule, isPlatformBrowser} from '@angular/common';
 import {HttpClient} from '@angular/common/http';
@@ -33,15 +33,9 @@ export class CodePresenter implements DoCheck {
   private readonly platformId = inject(PLATFORM_ID);
   private readonly isBrowser = isPlatformBrowser(this.platformId);
 
-  public readonly currentFileName = signal<string>('');
   protected renderCount = 0;
 
-  @Input({required: true})
-  set fileName(value: string) {
-    if (value && value !== this.currentFileName()) {
-      this.currentFileName.set(value);
-    }
-  }
+  fileName = input.required<string>();
 
   @Input() showRenderCount = false;
 
@@ -51,16 +45,27 @@ export class CodePresenter implements DoCheck {
     }
   }
 
+  // Use computed to set up language and filePath data when the fileName signal changes (emit new data)
+  private readonly fileInfo = computed(() => {
+    const fileName = this.fileName();
+    if (!fileName) {
+      return {filePath: '', language: DEFAULT_LANGUAGE};
+    }
+    const language = this.commonService.getLanguageFromFile(fileName);
+    const filePath = language === 'md'
+      ? `assets/content/markdown/${fileName}`
+      : `assets/content/code-samples/${fileName}`;
+    return {filePath, language};
+  });
+
   // Declare Observable to handle async data fetching file content
-  private readonly codeMarkdown$: Observable<string> = toObservable(this.currentFileName)
+  private readonly codeMarkdown$: Observable<string> = toObservable(this.fileInfo)
     .pipe(
-      switchMap(() => {
+      switchMap((info) => {
         // SSR/Prerender: don't fetch assets via HttpClient
         if (!this.isBrowser) {
           return EMPTY;
         }
-
-        const info = this.fileInfo();
 
         if (!info.filePath) {
           return of('');
@@ -69,7 +74,7 @@ export class CodePresenter implements DoCheck {
         return this.http.get(info.filePath, {responseType: 'text'})
           .pipe(
             map(codeContent => {
-              const isMarkdown = this.currentFileName().toLowerCase().endsWith('.md');
+              const isMarkdown = info.language === 'md';
               return isMarkdown
                 ? codeContent
                 : `\`\`\`${info.language}\n${codeContent}\n\`\`\``;
@@ -85,17 +90,4 @@ export class CodePresenter implements DoCheck {
       initialValue: 'Loading code...'
     }
   );
-
-  // Use computed to set up language and filePath data when the currentFileName signal changes (emit new data)
-  private readonly fileInfo = computed(() => {
-    const fileName = this.currentFileName();
-    if (!fileName) {
-      return {filePath: '', language: DEFAULT_LANGUAGE};
-    }
-    const language = this.commonService.getLanguageFromFile(fileName);
-    const filePath = language === 'md'
-      ? `assets/content/markdown/${fileName}`
-      : `assets/content/code-samples/${fileName}`;
-    return {filePath, language};
-  });
 }
